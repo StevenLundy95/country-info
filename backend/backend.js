814ef7ac75940b658d0faa581e6023ce0bc98519
StevenLundy95 <78603714+StevenLundy95@users.noreply.github.com>
2023-04-25 00:21:53 +0100

    commit 2

--- backend/backend.js
+++ backend/backend.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.use(express.json());
 
