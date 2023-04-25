ff07edd2047241fe88d0cc6ea860401463a9f346
StevenLundy95 <78603714+StevenLundy95@users.noreply.github.com>
2023-04-25 01:34:41 +0100

    commit

--- backend/backend.js
+++ backend/backend.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(express.json());
 
 app.get("/country/:name", async (req, res) => {
